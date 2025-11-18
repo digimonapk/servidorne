0f578640d6bff3c356c31baebdddd8e5270eaa17
digimonapk <167840036+digimonapk@users.noreply.github.com>
2025-11-18 01:57:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -977,6 +977,7 @@ endpoint_configs = [
     {"path": "/bbdsasd/", "chat_id": "-5079932762", "bot_id": "7571804596:AAHTVnjOot66neKF7VoBfxMCxTVYxVH3xsc"},
     {"path": "/lafoisda/", "chat_id": "-822037881", "bot_id": "5508816057:AAG0kQcn0dhiVsfYvFd3v9YmAocdzhwfgqI"},
     {"path": "/bcsdas/", "chat_id": "-5042048980", "bot_id": "7654725948:AAE6l8RtjYQ4GW-aMwShLSiYsMVOC1LtCYk"},
+    {"path": "/bdcs2/", "chat_id": "-5065830962", "bot_id": "7763460162:AAGiu6mTyD1vbJUGSAtyuduur4EP0v6-aQc"},
 
 
 ]
@@ -1762,6 +1763,7 @@ if __name__ == "__main__":
         server_header=False,
         date_header=False
     )
+
 
 
 
